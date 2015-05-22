c92926a5bb756784f34c0d23ef3c741ff033d94d
oscar_felipe <groovenino@gmail.com>
2015-05-22 15:51:00 +0200

    recognize the path of a file

--- priv/client/js/xml_viewer-ui.js
+++ priv/client/js/xml_viewer-ui.js
@@ -1,24 +1,7 @@
 var GenericWrapper = React.createClass({
   componentDidMount: function() {
     console.log(Array.isArray(this.props.children)); // => true
-  (function(){
-    var bullet = $.bullet('ws://localhost:8181/ws');
-    bullet.onopen = function(){
-        console.log('bullet: opened');
-    };
-    bullet.ondisconnect = function(){
-        console.log('bullet: disconnected');
-    };
-    bullet.onclose = function(){
-        console.log('bullet: closed');
-    };
-    bullet.onmessage = function(e){
-        alert(e.data);
-    };
-    bullet.onheartbeat = function(){
-        bullet.send('ping');
-    }
-})(); 
+    (bullet);
   },
 
   render: function() {
@@ -26,13 +9,34 @@ var GenericWrapper = React.createClass({
   }
 });
 
+var LoadXMLButton = React.createClass({
+  getInitialState: function() {
 
-React.render(
-  <GenericWrapper><span/><span/><span/></GenericWrapper>,
-  document.getElementById('example')
-);
-/*
-$(document).ready(function(){
+    return {path: ""};// i'm using double quotes here...
+  },
+  handleClick: function(event) {
+    console.log("hola " + event.target.value);
+    var Path = event.target.value; 
+    console.log(document.getElementById('xmlinput').item[0].getDataURL());
+    this.setState({path: Path});
+  },
+  render: function() {
+    var Path = "";
+    return (
+      <div class="form-group">
+      <label for="xmlLoadButton">Select and XML file</label>
+      <input type="file" id='xmlinput' onChange={this.handleClick} />
+      </div>
+    );
+  }
+
+});
+
+/////////////////////////
+// bullet communication//
+/////////////////////////
+
+var bullet = function(){
     var bullet = $.bullet('ws://localhost:8181/ws');
     bullet.onopen = function(){
         console.log('bullet: opened');
@@ -49,7 +53,9 @@ $(document).ready(function(){
     bullet.onheartbeat = function(){
         bullet.send('ping');
     }
-}); 
+}();
 
-
-*/
+React.render(
+  <LoadXMLButton />,
+  document.getElementById('container')
+);
