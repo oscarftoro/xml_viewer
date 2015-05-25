91a25df481b5752a78054ad8f4ca4dc75cd3b05e
oscar_felipe <groovenino@gmail.com>
2015-05-25 19:33:54 +0200

    change comment title

--- priv/client/js/xml_viewer-ui.js
+++ priv/client/js/xml_viewer-ui.js
@@ -1,7 +1,6 @@
 
 var LoadFileButton = React.createClass({
   componentDidMount: function() {
-
   },
   getInitialState: function() {
       var fileLoaded = false;
@@ -25,16 +24,152 @@ var LoadFileButton = React.createClass({
 
 });
 ///////////////////////
-//Auxiliar functions //
+//The Sacred D3 Tree //
 ///////////////////////
 
 function createTree(dom,props) {
-  d3.select(dom).append('h1').text("Ke paso");
-}
+  var margin = {top: 20, right: 120, bottom: 20, left: 120},
+  width      = props.width - margin.right - margin.left,
+  height     = props.height - margin.top - margin.bottom;
 
-/////////////////////////////////////
-/// The sacred D3.js Tree Wrapper //
-///////////////////////////////////
+  var i    = 0,
+  duration = 750,
+  root; //root is undefined
+
+  var tree = d3.layout.tree()
+      .size([height,width]);
+  var diagonal = d3.svg.diagonal()
+    .projection(function(d) { return [d.y, d.x]; });
+
+  var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.right + margin.left)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  //load data
+  root = props.data;
+  root.x0 = height / 2;
+  root.y0 = 0;
+
+  function collapse(d) {
+    if (d.children) {
+      d._children = d.children;
+      d._children.forEach(collapse);
+      d.children = null;
+    }
+  }
+
+  root.children.forEach(collapse);
+  update(root);
+  //end load of data - this was a function
+
+  d3.select(self.frameElement).style("height", "800px");
+
+  function update(source) {
+
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root).reverse(),
+        links = tree.links(nodes);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+    // Update the nodes\u2026
+    var node = svg.selectAll("g.node")
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+        .on("click", click);
+
+    nodeEnter.append("circle")
+        .attr("r", 1e-6)
+        .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeEnter.append("text")
+        .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1e-6);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+    nodeUpdate.select("circle")
+        .attr("r", 4.5)
+        .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeUpdate.select("text")
+        .style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+        .remove();
+
+    nodeExit.select("circle")
+        .attr("r", 1e-6);
+
+    nodeExit.select("text")
+        .style("fill-opacity", 1e-6);
+
+    // Update the links\u2026
+    var link = svg.selectAll("path.link")
+        .data(links, function(d) { return d.target.id; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function(d) {
+          var o = {x: source.x0, y: source.y0};
+          return diagonal({source: o, target: o});
+        });
+
+    // Transition links to their new position.
+    link.transition()
+        .duration(duration)
+        .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+        .duration(duration)
+        .attr("d", function(d) {
+          var o = {x: source.x, y: source.y};
+          return diagonal({source: o, target: o});
+        })
+        .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+    }
+
+    // Toggle children on click.
+    function click(d) {
+      if (d.children) {
+        d._children = d.children;
+        d.children = null;
+      } else {
+        d.children = d._children;
+        d._children = null;
+      }
+      update(d);
+    }
+
+  }//end update
+
+//////////////////////////////////////////
+/// The sacred D3's Tree React Wrapper //
+////////////////////////////////////////
 var Tree = React.createClass({
   propTypes: {
     width  : React.PropTypes.number,
@@ -42,8 +177,8 @@ var Tree = React.createClass({
   },
   getDefaultProps: function() {
     return {
-      width : 300,
-      height: 350,
+      width : 960,
+      height: 800,
     };
   },
   componentDidMount: function() {
@@ -104,9 +239,6 @@ var bullet = function(){
     }
 }();
 
-
-
-
 var data = {
  "name": "flare",
  "children": [
@@ -134,6 +266,7 @@ var data = {
     }
     ]
   }]};
+
 React.render(
 <div class="row">
   <div class="col-md-4">
